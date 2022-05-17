40108f9aa61e08bafc28e681725241692606a24e
Angela <angelamchoi@gmail.com>
2022-05-17 11:48:36 -0400

    added notes for user and micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,4 @@
 class Micropost < ApplicationRecord
+  belongs_to :user # A post belongs to a user
+  validates :content, length: {maximum: 140} # validation        
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :microposts
 end
+
+
+# Notes
+#--since a user has many posts we can show the relationship by --> has_many
