ac6a5c131c8973778aa471c030e86bfd25f2a4db
Angela <angelamchoi@gmail.com>
2022-05-26 22:00:15 -0400

    fixed editing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController #inheritance
-  
+  # before_action :logged_in_user, only: [:edit, :update]
+  # before_action :correct_user, only: [:edit, :update]
 
 
   # GET /users or /users.json
@@ -54,7 +55,12 @@ private
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
   
-
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 
   # DELETE /users/1 or /users/1.json
   def destroy
